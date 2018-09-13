f1e28affb31194dbd207c2c5cd73b99d0bf322ed
Milda Poceviciute <milpo192@student.liu.se>
2018-09-13 14:20:14 +0200

    Milda code

---
+++ lab1code_Milda.R
@@ -0,0 +1,142 @@
+# Advanced machine learning
+
+#Lab 1
+
+library(bnlearn)
+library(gRain) 
+library(RBGL)
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("RBGL")
+library(gRbase)
+
+#Questio 1
+data("asia")
+summary(asia)
+
+BN1<-hc(asia)
+summary(BN1)
+plot(BN1)
+arcs(BN1)
+vstructs(BN1)
+cpdag(BN1)
+
+BN2 <- hc(asia, start = NULL, restart = 10)
+plot(BN2)
+cpdag(BN2)
+
+#The imaginary or equivalent sample size of the prior distribution can be specified using the iss parameter; 
+#it specifies the weight of the prior compared to the sample and thus controls the smoothness of the posterior distribution.
+
+BN3 <- hc(asia, start = NULL, score = "bde", restart = 2, iss=10)
+plot(BN3)
+cpdag(BN3)
+
+BN4 <- hc(asia, start = NULL, score = "bde", restart = 2, iss=100)
+plot(BN43)
+cpdag(BN4)
+
+BN5 <- hc(asia, start = NULL, score = "bde", restart = 5, iss=50)
+plot(BN5)
+cpdag(BN5)
+
+BN6 <- hc(asia, start = NULL, score = "bde", restart = 10, iss=50)
+plot(BN6)
+cpdag(BN6)
+
+cnames <- colnames(asia)
+startg <- random.graph(nodes=cnames, num=1, method="melancon")
+plot(startg)
+BN7 <- hc(asia, start = startg)
+plot(BN7)
+
+BN8 <- hc(asia, start = startg, score = "bde", restart = 5, iss=10)
+plot(BN8)
+
+all.equal(BN8, BN2) 
+all.equal(BN8, BN4)
+all.equal(BN1,BN7) 
+all.equal(BN1, BN6) 
+all.equal(BN1,BN2)
+all.equal(BN2,BN3)
+all.equal(BN2, BN4)
+all.equal(BN3,BN4)
+all.equal(BN1, BN3)
+all.equal(BN1,BN4)
+all.equal(BN3, BN5)
+all.equal(BN2, BN5)
+all.equal(BN4,BN5)
+all.equal(BN1, BN5)
+
+
+# Question 2
+N <- nrow(asia)
+ind <- sample(1:N, size = 0.8*N)
+train <- asia[ind,]
+test <- asia[-ind,]
+
+train_learn <- hc(train, start = NULL, restart = 10)
+summary(train_learn)
+plot(train_learn)
+
+# Learn parameters using ML
+ml_fit <- bn.fit(train_learn, train, method = "mle")
+ml_fit
+bn.fit.barchart(ml_fit$A)
+bn.fit.barchart(ml_fit$S)
+bn.fit.dotplot(ml_fit$S)
+
+
+
+# Learn parameters using Bayes method
+bayes_fit = bn.fit(train_learn, train, method = "bayes")
+bayes_fit
+bn.fit.barchart(bayes_fit$A)
+bn.fit.barchart(bayes_fit$S)
+bn.fit.dotplot(bayes_fit$S)
+
+# As S has no parents, the porbabilities are close to 0,5
+
+
+# Using grain package"
+ml_grain <- as.grain(ml_fit)
+
+compile(ml_grain) 
+setFinding(ml_grain)
+querygrain(ml_grain, nodes = nodeNames(ml_grain)) # marginal prob
+
+##################################
+# Do we need to come up with our own evidence?
+
+
+##################################
+## Prediction based on evidence
+cnames2 <- colnames(test)
+
+Ev1 <- setEvidence(ml_grain,nodes=cnames2[-2],states=test[1,-2]) # S is no
+pEvidence(Ev1)
+S_yes <- test[1,]
+S_yes$S <-test[2,2] # S is yes
+Ev2 <- setEvidence(ml_grain,nodes=cnames2,states=S_yes)
+pEvidence(Ev2)
+
+
+plot(ml_fit)
+
+# Prediction Uniform
+prob_no <- ml_fit$S$prob["no"]
+s <- vector(length = nrow(test))
+for(i in 1:nrow(test)){
+  if(runif(1)<prob_no){
+    s[i] <- "no"
+  }else{
+    s[i] <- "yes"
+  }
+}
+# Confusion matrix
+table(test$S, s)
+
+dag = model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]") #true graph
+plot(dag)
+arcs(dag)
+arcs(hc1)
+plot(hc1)
